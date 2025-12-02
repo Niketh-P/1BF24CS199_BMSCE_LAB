9156f2089ef3767e2616406d4b99a90a172f5688
Niketh-P <nikethp.cs24@bmsce.ac.in>
2025-12-02 06:47:12 +0530

    Create 1BF24CS199_WEEK7(b).C

---
+++ DS/1BF24CS199_WEEK7(b).C
@@ -0,0 +1,180 @@
+//STACK IMPLEMENTATION
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;   // Stack top pointer
+
+void push(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data=val;
+    if (top==NULL) {
+        newNode->next=NULL;
+        top=newNode;
+    }
+    else {
+        newNode->next=top;
+        top=newNode;
+    }
+
+}
+
+void pop() {
+    if (top==NULL) {
+        printf("Empty");
+        return;
+    }
+    struct Node* temp=top;
+    printf("The popped element is : %d ",top->data);
+    top=top->next;
+    free(temp);
+}
+
+
+void displayStack() {
+    struct Node* temp = top;
+
+    if (temp == NULL) {
+        printf("Stack is Empty\n");
+        return;
+    }
+
+    printf("Stack elements:\n");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    int ch, val;
+
+    while (1) {
+        printf("\nSTACK MENU\n");
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                push(val);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+
+            case 3:
+                displayStack();
+                break;
+
+            case 4:
+                exit(0);
+
+
+//QUEUE IMPLEMENTATION
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;   // Front of queue
+struct Node* rear  = NULL;   // Rear of queue
+
+void enqueue(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if (front == NULL) {   // Queue empty
+        front = rear = newNode;
+    }
+    else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    printf("Inserted %d\n", val);
+}
+
+void dequeue() {
+    if (front == NULL) {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("Deleted: %d\n", front->data);
+    front = front->next;
+
+    if (front == NULL) {
+        rear = NULL;
+    }
+
+    free(temp);
+}
+
+void displayQueue() {
+    if (front == NULL) {
+        printf("Queue is Empty\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("Queue: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    int ch, val;
+
+    while (1) {
+        printf("\nQUEUE MENU\n");
+        printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &val);
+                enqueue(val);
+                break;
+
+            case 2:
+                dequeue();
+                break;
+
+            case 3:
+                displayQueue();
+                break;
+
+            case 4:
+                exit(0);
+
+            default:
+                printf("Invalid Choice\n");
+        }
+    }
+}
+
+            default:
+                printf("Invalid Choice\n");
+        }
+    }
+}
